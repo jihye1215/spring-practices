8c9d15d3716fa439295b7f069b8375df7070363e
wlgp1 <wlgp1@DESKTOP-5T5NIVG>
2022-01-21 15:14:54 +0900

    guestbook03 finish

--- guestbook03/src/main/java/com/poscoict/guestbook/controller/GuestbookController.java
+++ guestbook03/src/main/java/com/poscoict/guestbook/controller/GuestbookController.java
@@ -1,15 +1,45 @@
 package com.poscoict.guestbook.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.poscoict.guestbook.repository.GuestbookRepository;
+import com.poscoict.guestbook.vo.GuestbookVo;
 
 @Controller
 public class GuestbookController {
+	@Autowired
+	private GuestbookRepository guestbookRepository;
 	
-	@ResponseBody
 	@RequestMapping("")
-	public String index() {
-		return "guestbook";
+	public String index(Model model) {
+		List<GuestbookVo> list = new GuestbookRepository().selectAll();
+		model.addAttribute("list", list);
+		return "/WEB-INF/views/index.jsp";
+	}
+	
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String add(GuestbookVo vo) {
+		guestbookRepository.insert(vo);
+		return "redirect:/";
+	}
+	
+	@RequestMapping("/deleteform/{no}")
+	public String delete(@PathVariable("no") Long no,Model model) {
+		model.addAttribute("no",no);
+		return "/WEB-INF/views/deleteform.jsp";
+	}
+	
+	@RequestMapping(value = "/delete", method = RequestMethod.POST)
+	public String delete(Long no, String password) {
+		guestbookRepository.delete(no, password);
+		return "redirect:/";
 	}
 }
---
+++ guestbook03/src/main/java/com/poscoict/guestbook/repository/GuestbookRepository.java
@@ -0,0 +1,204 @@
+package com.poscoict.guestbook.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.poscoict.guestbook.vo.GuestbookVo;
+
+@Repository
+public class GuestbookRepository {
+   public List<GuestbookVo> selectAll() {
+      List<GuestbookVo> result = new ArrayList();
+      Connection conn = null;
+      PreparedStatement pstmt = null;
+      ResultSet rs = null;
+      
+      try {
+         
+         conn = getConnection();
+         
+         String sql = "select no, name, date_format(reg_date, '%Y/%m/%d %H:%i:%s') as reg_date, message from guestbook order by reg_date desc";
+         pstmt = conn.prepareStatement(sql);
+         rs = pstmt.executeQuery();
+         
+         while(rs.next()) {
+            Long no = rs.getLong(1);
+            String name = rs.getString(2);
+            String reg_date = rs.getString(3);
+            String message = rs.getString(4);
+            
+            GuestbookVo vo = new GuestbookVo();
+            vo.setNo(no);
+            vo.setName(name);
+            vo.setReg_date(reg_date);
+            vo.setMessage(message);
+            result.add(vo);
+         }
+      } catch(SQLException e) {
+         System.out.println("error : " + e);
+      } finally {
+         // 자원 정리
+         try {
+            if(rs != null) {
+               rs.close();
+            }
+            if(pstmt != null) {
+               pstmt.close();
+            }
+            if(conn != null) {
+               conn.close();
+            }
+         } catch (SQLException e) {
+            e.printStackTrace();
+         }
+      }
+      return result;
+      
+   }
+   
+   public boolean insert(GuestbookVo vo) {
+      boolean result = false;
+      Connection conn = null;
+      PreparedStatement pstmt = null;
+      ResultSet rs = null;
+      
+      try {
+         conn = getConnection();
+         
+         String sql = "insert into guestbook values(null, ?, ?, ?, now())";
+         pstmt = conn.prepareStatement(sql);
+         
+         pstmt.setString(1,  vo.getName());
+         pstmt.setString(2, vo.getPassword());
+         pstmt.setString(3, vo.getMessage());
+         
+         result = (pstmt.executeUpdate() == 1);
+      } catch(SQLException e) {
+         System.out.print("error : "+e);
+      }finally {
+         // 자원 정리
+         try {
+            if(rs != null) {
+               rs.close();
+            }
+            if(pstmt != null) {
+               pstmt.close();
+            }
+            if(conn != null) {
+               conn.close();
+            }
+         } catch (SQLException e) {
+            e.printStackTrace();
+         }
+      }
+      return result;
+   }
+   
+   public boolean delete(Long no, String password) {
+	      
+	      boolean result = false;
+	      Connection conn = null;
+	      PreparedStatement pstmt = null;
+	      ResultSet rs = null;
+	      
+	      try {
+	         conn = getConnection();
+	         
+	         //3. SQL 준비
+	         String sql = "delete from guestbook where no = ? and password = ?";
+	         pstmt = conn.prepareStatement(sql);
+
+	         //4. 바인딩(binding)   
+	         pstmt.setLong(1, no);
+	         pstmt.setString(2, password);
+	         
+	         //5. SQL 실행 , executeQuery는 rs, executeUpdate는 int로 반환한다. 
+	         result = (pstmt.executeUpdate() == 1);
+  
+	      } catch (SQLException e) {
+	         System.out.print("error : " + e); 
+	      }
+	      
+	      finally {
+	         try {
+	            if(rs != null) {
+	               rs.close();
+	            }
+	            if(pstmt != null) {
+	               pstmt.close();
+	            }
+	            if(conn != null) {
+	               conn.close();
+	            }
+	         } catch(SQLException e) {
+	            e.printStackTrace();
+	         }
+	      }
+	      
+	      return result;
+	   }
+	   
+   public int selectCnt(){
+	      
+	      Connection conn = null;
+	      PreparedStatement pstmt = null;
+	      ResultSet rs = null;
+	      int result = 0;
+	      
+	      try {
+	         conn = getConnection();
+
+	         String sql = "select count(*) from guestbook";
+	         pstmt = conn.prepareStatement(sql);
+
+	         rs = pstmt.executeQuery();
+
+	         if(rs.next()) {
+	            result = rs.getInt(1);
+	         }
+	         
+	      } catch (SQLException e) {
+	         System.out.print("error : " + e); 
+	      }
+	      
+	      finally {
+	         try {
+	            if(rs != null) {
+	               rs.close();
+	            }
+	            if(pstmt != null) {
+	               pstmt.close();
+	            }
+	            if(conn != null) {
+	               conn.close();
+	            }
+	         } catch(SQLException e) {
+	            e.printStackTrace();
+	         }
+	      }
+	      return result;
+	   }
+
+   
+   private Connection getConnection() throws SQLException {
+      Connection conn = null;
+      try {
+         // 1. JDBC 드라이버 로딩
+         Class.forName("com.mysql.cj.jdbc.Driver");
+         // 2. 연결하기
+         String url = "jdbc:mysql://localhost:3306/webdb?characterEncoding=UTF-8&serverTimezone=UTC";
+         conn = DriverManager.getConnection(url, "webdb", "webdb");
+      } catch(ClassNotFoundException e) {
+         System.out.print("드라이버 로딩 실패 : "+e);
+      }
+      return conn;
+   }
+
+}
