8c6c7fb0b5113c4b6f356ce0a209cb52f5b98081
wlgp1 <wlgp1@DESKTOP-5T5NIVG>
2022-01-28 16:36:15 +0900

    tomcat 플러그인 설정

--- fileupload/src/main/java/com/poscoict/fileupload/service/FileUploadService.java
+++ fileupload/src/main/java/com/poscoict/fileupload/service/FileUploadService.java
@@ -10,59 +10,54 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class FileUploadService {
+	
+	private static String SAVE_PATH = "/upload-images";
+	private static String URL_BASE = "/images";
 
-   private static final String SAVE_PATH = "/upload-images";
-   private static final String URL_BASE = "/images";
-   
-   public String restore(MultipartFile multipartFile) {
-      String url = null;
-      
-      try {
-      if(multipartFile.isEmpty()) {
-         return url;
-      }
-      
-      String originFileName = multipartFile.getOriginalFilename();
-      String extName01 = originFileName.substring(originFileName.lastIndexOf('.'));
-      String extName02 = originFileName.substring(originFileName.lastIndexOf('.') + 1);
-      String saveFileName = generateSaveFileName(extName02);
-      long fileSize = multipartFile.getSize();
-      System.out.println("# originFileName : " + originFileName);
-      System.out.println("# fileSize : " + fileSize);
-      System.out.println("# extName01 : " + extName01);
-      System.out.println("# extName02 : " + extName02);
-      System.out.println("# saveFileName : " + saveFileName);
-      
-      
-      byte[] data = multipartFile.getBytes(); // error catch
-      
-      OutputStream os = new FileOutputStream(SAVE_PATH + "/" + saveFileName);
-      os.write(data);
-      os.close();
-      
-      url = URL_BASE + "/" + saveFileName;
-      
-      } catch(IOException ex) {
-         throw new RuntimeException("file upload error : " + ex);
-      }
-      
-      return url;
-   }
+	public String restore(MultipartFile multipartFile) {
+		String url = null;
+		try {
+			if(multipartFile.isEmpty()) {
+				return url;
+			}
+			
+			String originFileName = multipartFile.getOriginalFilename();
+			String extName = originFileName.substring(originFileName.lastIndexOf('.')+1);
+			String saveFileName = generateSaveFilename(extName);
+			long fileSize = multipartFile.getSize();
+			
+			System.out.println("###############" + originFileName);
+			System.out.println("###############" + saveFileName);
+			System.out.println("###############" + fileSize);
+			
+			byte[] data = multipartFile.getBytes();
+			
+			OutputStream os = new FileOutputStream(SAVE_PATH + "/" + saveFileName);
+			os.write(data);
+			os.close();
+			
+			url = URL_BASE + "/" + saveFileName;
+			
+		} catch(IOException ex) {
+			throw new RuntimeException("file upload error : " + ex);
+		}
+		return url;
+	}
 
-   private String generateSaveFileName(String extName) {
-      String filename = "";
-      Calendar calendar = Calendar.getInstance(); // new 필요하지 않음
 
-      filename += calendar.get(Calendar.YEAR);
-      filename += calendar.get(Calendar.MONTH);
-      filename += calendar.get(Calendar.DATE);
-      filename += calendar.get(Calendar.HOUR);
-      filename += calendar.get(Calendar.MINUTE);
-      filename += calendar.get(Calendar.SECOND);
-      filename += calendar.get(Calendar.MILLISECOND);
-      filename += "." + extName;
-      
-      return filename;
-   }
+	private String generateSaveFilename(String extName) {
+		String filename = "";
+		Calendar calendar = Calendar.getInstance();
+		
+		filename += calendar.get(Calendar.YEAR);
+		filename += calendar.get(Calendar.MONTH);
+		filename += calendar.get(Calendar.DATE);
+		filename += calendar.get(Calendar.HOUR);
+		filename += calendar.get(Calendar.MINUTE);
+		filename += calendar.get(Calendar.SECOND);
+		filename += calendar.get(Calendar.MILLISECOND);
+		filename += ("." + extName);
+		return filename;
+	}
 
 }
